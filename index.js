a32448a17be69c4eb9147cdee9ba16751b053366
farhod <farhod.jonikulovich@gmail.com>
2024-10-06 16:38:17 +0500

    changed main func

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-import { fork } from "child_process";
-
 const main = () => {
   const args = process.argv.slice(2);
 
-  const childProcess = fork("./script.js", args, {
-    silent: true,
+  const userName = args?.[0]?.split("=")[1];
+  if (userName) console.log(`Welcome to the File Manager, ${userName}!`);
+
+  const exitGracefully = () => {
+    console.log(`Thank you for using File Manager, ${userName}, goodbye!`);
+    process.exit();
+  };
+
+  process.stdin.on("data", (data) => {
+    if (data.includes(".exit")) {
+      exitGracefully();
+    }
+    process.stdout.write(`Received from master process: ${data.toString()}\n`);
   });
 
-  process.stdin.pipe(childProcess.stdin);
-  childProcess.stdout.pipe(process.stdout);
+  process.on("SIGINT", () => {
+    exitGracefully();
+  });
 };
 
 main();
