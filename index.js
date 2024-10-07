c2c92171237fa1759f608fd1a8fb09a8f346780b
farhod <farhod.jonikulovich@gmail.com>
2024-10-07 18:02:21 +0500

    File manager in progress

--- index.js
+++ index.js
@@ -1,19 +1,54 @@
+import path from "path";
+import fs from "fs";
+
 const main = () => {
   const args = process.argv.slice(2);
 
   const userName = args?.[0]?.split("=")[1];
-  if (userName) console.log(`Welcome to the File Manager, ${userName}!`);
+  if (userName) console.log(`Welcome to the File Manager, ${userName}!\n`);
+  console.log(`You are currently in ${process.cwd()}\n`);
 
   const exitGracefully = () => {
     console.log(`Thank you for using File Manager, ${userName}, goodbye!`);
     process.exit();
   };
 
-  process.stdin.on("data", (data) => {
+  process.stdin.on("data", async (data) => {
+    if (data.includes("up")) {
+      const upperDir = path.join(process.cwd(), "..");
+      process.chdir(upperDir);
+    }
+
+    if (data.includes("cd")) {
+      const dirName = data.toString().slice(3).trim();
+
+      if (!dirName) {
+        console.error("Error: No directory specified.");
+        return;
+      }
+
+      const openDirPath = path.isAbsolute(dirName)
+        ? dirName
+        : path.join(process.cwd(), dirName);
+
+      fs.stat(openDirPath, (err, stats) => {
+        if (err || !stats.isDirectory()) {
+          console.error(`Operation failed`);
+        } else {
+          try {
+            process.chdir(openDirPath);
+            console.log(`You are currently in ${process.cwd()}\n`);
+          } catch (err) {
+            console.error(`Error: Unable to change to directory '${dirName}'`);
+          }
+        }
+      });
+    }
+    console.log(`You are currently in ${process.cwd()}\n`);
+
     if (data.includes(".exit")) {
       exitGracefully();
     }
-    process.stdout.write(`Received from master process: ${data.toString()}\n`);
   });
 
   process.on("SIGINT", () => {
